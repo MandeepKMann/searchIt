f342acd042012fa8f8370884907e3163885a6fdf
MandeepMann <mandeep_mann30@hotmail.com>
2023-02-04 16:08:33 -0500

    finished js, just needs styling

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 const searchIt = {};
 
 searchIt.googleSearch = () => {
+    // select textarea and data inside, then change spaces to + and separate data by line
+    const textElement = document.querySelector('textarea');
+    const userSearch = textElement.value.replaceAll(' ', '+').split("\n");
 
+    // add event listener to the form
     const formElement = document.querySelector('#search')
-    formElement.addEventListener('submit', function (e) {
+    formElement.addEventListener('submit', (e) => {
         e.preventDefault();
+        // append links to the page for each search and replace '+' back to ' ' to make it easier to read
+        userSearch.forEach(search => {
+            const newLi = document.createElement('li')
+            newLi.innerHTML = `<a href="https://www.google.com/search?q=${search}">${search.replaceAll('+', ' ')}</a>`
+            const linksList = document.querySelector('.links')
+            linksList.appendChild(newLi)
+        });
+    });
 
-        const textElement = document.querySelector('textarea');
-        const userSearch = textElement.value.replaceAll(' ', '+').split("\n");
-        console.log(userSearch)
-
-        // userSearch.forEach(search => {
-        //     const newLi = document.createElement('li')
-        //     newLi.innerHTML = `<a href="https://www.google.com/search?q=${search}">Click Me!</a>`
-        //     const linksList = document.querySelector('.links')
-        //     linksList.appendChild(newLi)
-        // });
-        
+    // add event listener to button to allow it to open all links
+    const openButton = document.querySelector('.openAll');
+    openButton.addEventListener('click', (e) => {
+        // for loop to iterate through array of strings to be searched
         for (let i = 0; i < userSearch.length; i++) {
             window.open(`https://www.google.com/search?q=${userSearch[i]}`)
         };
-        
-
-    })
-
-    // const url = `https://www.google.com/search?${userSearch}`
+    });
 }
-
-//https://www.google.com/search?q=29+baylawn+circle
-
-
 
 // Init
 searchIt.init = () => {
